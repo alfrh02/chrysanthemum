ca1d8bbe851528e83854388b99625e43f982d63b
alfrh02 <alfred.n.hall@gmail.com>
2023-12-26 12:17:38 +0000

    initial commit

---
+++ src/player/player.cpp
@@ -0,0 +1,15 @@
+#include "player.h"
+
+Player::Player() {
+    cout << "Creating Player" << endl;
+}
+
+Player::~Player() {
+    cout << "Destroying Player" << endl;
+}
+
+void Player::draw() {
+}
+
+void Player::update() {
+}
---
+++ src/player/player.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ofMain.h"
+
+class Player {
+    public:
+        Player();
+        ~Player();
+
+        void draw();
+        void update();
+};
